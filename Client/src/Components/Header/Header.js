34e90029165f7fc690cfdbf548ccc2a9b62af12a
kieukhanhquan <kieukhanhquan@gmail.com>
2023-03-29 12:47:27 +0700

    add Header-Client component

--- Client/src/Components/Header/Header.js
+++ Client/src/Components/Header/Header.js
@@ -1,14 +1,9 @@
 import { useLayoutEffect, useState } from "react"
 import { useLocation } from "react-router-dom"
-// import logo from "../../Assets/avatar.png";
-// import { Dropdown } from 'rsuite';
-import PeoplesIcon from '@rsuite/icons/Peoples'
-import DashboardIcon from '@rsuite/icons/legacy/Dashboard'
-import GearCircleIcon from '@rsuite/icons/legacy/GearCircle'
-import MessageIcon from '@rsuite/icons/Message'
-import TagNumberIcon from '@rsuite/icons/TagNumber'
-import DetailIcon from '@rsuite/icons/Detail';
-import MenuIcon from '@rsuite/icons/Menu';
+import SearchIcon from '@rsuite/icons/Search';
+import PersonIcon from '@mui/icons-material/Person';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 // import { Whisper, Avatar, Popover } from 'rsuite';
 import "./header.css";
 
@@ -18,29 +13,39 @@ function Header(){
     
     return (
         <header className="header">
-            <div className="navbar1">
-                <a href='./index.php' className="navbar1__logo">LOGO</a>
-                <div className="navbar1__wrap">
-                    <div className="navbar1__cart">
-                        <i className="navbar1__cart-icon fa-solid fa-cart-shopping"></i>
-                        <a href="./index.php?url=Login" className="navbar1__cart-link">Giỏ hàng</a>
+            <div className="navbar">
+                <a href='./index.php' className="navbar_logo">LOGO</a>
+                <div className="categories">
+                    <div className="categories_wrap_link">
+                        <a href="" className="categories_link">Khuyến mãi</a>
                     </div>
-                    <a href="./index.php?url=Login" className="navbar1__login">Đăng nhập</a>
-                    <div className="navbar1__social">
-                        <a href="" className="navbar1__social-item">
-                            <i className="navbar1__social-icon_fb fa-brands fa-facebook"></i>
-                        </a>
-                        <a href="" className="navbar1__social-item">
-                            <i className="navbar1__social-icon_insta fa-brands fa-instagram"></i>
-                        </a>
+                    <div className="categories_wrap_link">
+                        <a href="" className="categories_link">Hot</a>
+                    </div>
+                    <div className="categories_wrap_link">
+                        <a href="" className="categories_link">Mới ra mắt</a>
+                    </div>
+                    <div className="categories_wrap_link">
+                        <a href="" className="categories_link">Phụ Kiện</a>
                     </div>
                 </div>
-                <div className="navbar1__search">
-                    <input type="" className="navbar1__search-text" placeholder='Nhập từ khóa tìm kiếm' />
-                    <div className="navbar1__search-wrap">
-                        <i className="navbar1__search-icon fa-solid fa-magnifying-glass"></i>
-                    </div>
-
+                
+                <form action="/search" method="get" className="navbar_search">
+                    <input type="text" className="navbar__search-text" placeholder='Nhập từ khóa tìm kiếm' />
+                    <button type="submit" className="navbar__search-icon">
+                        <SearchIcon height= "90%"/>
+                    </button>
+                </form>
+                <div className="route_wrap">
+                    <a href=""  className="route-icon">
+                            <PersonIcon height= "90%"/>
+                    </a>
+                    <a href=""  className="route-icon">
+                            <ShoppingCartIcon height= "90%"/>
+                    </a>
+                    <a href=""  className="route-icon">
+                            <FavoriteBorderIcon height= "90%"/>
+                    </a>
                 </div>
             </div>
 
