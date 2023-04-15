a343f45b689b359d827c9454f4f6b942e204ab4c
kieukhanhquan <kieukhanhquan@gmail.com>
2023-04-15 22:07:35 +0700

    update

--- Client/src/Redux/Reducers/store.js
+++ Client/src/Redux/Reducers/store.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 import { configureStore } from '@reduxjs/toolkit'
-import productReducer from "./Slice/productSlice";
+import productSlice from "./Slice/productSlice";
 
 export const store = configureStore({
   reducer:{
